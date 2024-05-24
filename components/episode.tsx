fca72d525492fe2491f0b72dead473ed455f4ae1
foxbit19 <foxbit19@users.noreply.github.com>
2024-05-25 00:21:48 +0200

    Add fireworks

--- components/episode.tsx
+++ components/episode.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { Button } from '@nextui-org/button'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
+import { TOnInitPresetFn, TRunAnimationParams } from 'react-canvas-confetti/dist/types';
 
 type Props = {
     number: number | string
@@ -8,13 +10,23 @@ type Props = {
 
 const Episode = (props: Props) => {
     const [variant, setVariant] = useState<'faded' | 'shadow'>('faded')
+    const controller = useRef<any>();
+
+    const onInitHandler = ({ conductor }: any) => {
+        controller.current = conductor;
+    };
 
     const handleClick = () => {
         setVariant(variant === 'shadow' ? 'faded' : 'shadow')
+        controller.current.shoot();
+
     }
 
     return (
-        <Button color='primary' variant={variant} onClick={handleClick}>{props.number}</Button>
+        <div>
+            <Fireworks onInit={onInitHandler} />
+            <Button color='primary' radius='none' variant={variant} onClick={handleClick}>{props.number}</Button>
+        </div>
     )
 }
 
